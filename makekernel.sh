7d5fafba5c73a38819988fb019832f5dac8819fd
DragonZX <DragonZX@yandex.ru>
2014-09-27 17:33:45 +0400

    Disabled comments

--- makekernel.sh
+++ makekernel.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-ver=3.16.2
+ver=3.16.3
 # uncomment it if you don't have developer packages
-#apt-get -y install kernel-package libncurses5-dev fakeroot wget build-essential bc
+apt-get -y install make kernel-package libncurses5-dev fakeroot wget build-essential bc
 echo Installing $ver
 cd /usr/src
-#wget kernel.org/pub/linux/kernel/v3.x/linux-$ver.tar.xz
+wget kernel.org/pub/linux/kernel/v3.x/linux-$ver.tar.xz
 tar xvf linux-$ver.tar.xz
 rm linux
 ln -s linux-$ver linux
 cd /usr/src/linux
 cp /boot/config-`uname -r` ./.config
 make oldconfig
-# make menuconfig
+make menuconfig
 # uncomment it if you want to reconfigure the kernel
 make-kpkg clean
 fakeroot make-kpkg --initrd kernel_image kernel_headers kernel_doc kernel_source
