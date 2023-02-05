b3441760d8f4de2c1b14e8a9e9167e5bdce582fa
DragonZX <dragonzx@yandex.ru>
2023-02-05 16:46:48 +0300

    armbian presets

---
+++ armbian-preset/opweb.sh
@@ -0,0 +1,3 @@
+# cockpit-sosreport is conflicts
+apt install cockpit-bridge cockpit-pcp cockpit-networkmanager cockpit-packagekit cockpit-storaged cockpit-ws
+apt install nginx php-fpm uwsgi
---
+++ armbian-preset/zxgames.sh
@@ -0,0 +1,4 @@
+apt install dosbox fuse-emulator-gtk fuse-emulator-utils spectrum-roms
+apt install snapd
+snap install core
+snap install sdlpop
