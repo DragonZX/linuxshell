11f3a29bb3bf7aaa710f0addda8d42e748112c4b
DragonZX <dragonzx@yandex.ru>
2023-02-23 15:32:47 +0300

    Full emulators preset

--- armbian-preset/zxgames.sh
+++ armbian-preset/zxgames.sh
@@ -1,4 +1,8 @@
+add-apt-repository ppa:libretro/stable
+apt update
 apt install dosbox fuse-emulator-gtk fuse-emulator-utils spectrum-roms
+apt install mednafen mednaffe nestopia xboxdrv
+apt-get install retroarch retroarch-* libretro-*
 apt install snapd
 snap install core
 snap install sdlpop
