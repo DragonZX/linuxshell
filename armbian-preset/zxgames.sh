c673dde5d735b0dc2c3794118d47b4aa23aee501
DragonZX <dragonzx@yandex.ru>
2023-03-01 18:51:27 +0300

    mame from ppa

--- armbian-preset/zxgames.sh
+++ armbian-preset/zxgames.sh
@@ -1,9 +1,10 @@
 add-apt-repository ppa:libretro/stable
+add-apt-repository ppa:c.falco/mame
 apt update
 apt install dosbox fuse-emulator-gtk fuse-emulator-utils spectrum-roms
 apt install mednafen mednaffe nestopia xboxdrv
 apt install retroarch retroarch-* libretro-*
-apt install mame mame-tools gnome-video-arcade
+apt install mame mame-doc mame-extra mame-tools gnome-video-arcade
 apt install snapd
 snap install core
 snap install sdlpop
