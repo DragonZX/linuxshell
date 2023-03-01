4286ccaf72f122b3af28e21c5494e054b0061e6e
DragonZX <dragonzx@yandex.ru>
2023-03-01 12:35:38 +0300

    Added mame to the list

--- armbian-preset/zxgames.sh
+++ armbian-preset/zxgames.sh
@@ -2,7 +2,8 @@ add-apt-repository ppa:libretro/stable
 apt update
 apt install dosbox fuse-emulator-gtk fuse-emulator-utils spectrum-roms
 apt install mednafen mednaffe nestopia xboxdrv
-apt-get install retroarch retroarch-* libretro-*
+apt install retroarch retroarch-* libretro-*
+apt install mame mame-tools gnome-video-arcade
 apt install snapd
 snap install core
 snap install sdlpop
