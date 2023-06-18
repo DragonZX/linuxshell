235ebd9e1bb4213452451e435f157d4d69ad65cb
Vitaliy Zhukov <DragonZX@users.noreply.github.com>
2023-06-18 16:36:01 +0300

    Update opweb.sh
    System needed for Bookworm

--- armbian-preset/opweb.sh
+++ armbian-preset/opweb.sh
@@ -1,3 +1,3 @@
 # cockpit-sosreport is conflicts
-apt install cockpit-bridge cockpit-pcp cockpit-networkmanager cockpit-packagekit cockpit-storaged cockpit-ws
+apt install cockpit-system cockpit-bridge cockpit-pcp cockpit-networkmanager cockpit-packagekit cockpit-storaged cockpit-ws
 apt install nginx php-fpm uwsgi
